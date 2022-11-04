0c1008da4afc46769259430b2a4d6ac0492d69fa
Bharanilikith <Bharanilikith@Bharani>
2022-11-04 17:08:38 +0530

    hi

---
+++ src/test/java/scripts/Test7.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test7 {
+	@Test
+	public void tc1() {
+		Reporter.log("Hello Restassured",true);
+	}
+
+}
---
+++ src/test/java/scripts/Test8.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test8 {
+	@Test
+	public void tc1() {
+		Reporter.log("Hello Testcomplete",true);
+	}
+
+}
